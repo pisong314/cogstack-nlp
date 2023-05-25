07350b40eba4aa687bcc2c5b932b1434572511c2
antsh3k <antshek@hotmail.com>
2023-05-25 21:09:52 +0100

    rename anoncat to api

--- anoncat-demo-app/anoncat/api/settings.py
+++ anoncat-demo-app/anoncat/api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'anoncat.urls'
+ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'anoncat.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.application'
 
 
 # Database
