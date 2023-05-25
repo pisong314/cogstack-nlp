c2a8e12c62fde96d1171cf1ea11d48cd78a2ace5
antsh3k <antshek@hotmail.com>
2023-05-25 23:04:37 +0100

    removed redundant code

--- anoncat-demo-app/anoncat/deidentify_app/views.py
+++ anoncat-demo-app/anoncat/deidentify_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.conf import settings
 from .forms import DeidentifyForm
 from .models import DeidentifiedText
+import os
 from medcat.cat import CAT
 from medcat.utils.ner import deid_text
 
-cat = CAT.load_model_pack("/Users/anthonyshek/projects/deidentify/anoncat/deidentify_app/models/deid_medcat_n2c2_modelpack.zip") # Load a model
+
+cat = CAT.load_model_pack(settings.DEID_MODEL) # Load a model
 
 # Create your views here.
 def deidentify(request):
@@ -28,25 +31,3 @@ def deidentify(request):
         form = DeidentifyForm()
         return render(request, 'deidentify_demo.html', {'form': form})
 
-
-def basic_deidentify(request):
-    if request.method == 'POST':
-        form = DeidentifyForm(request.POST)
-        if form.is_valid():
-            input_text = form.cleaned_data['input_text']
-            # Deidentify the input_text here using a simple method
-            output_text = ""
-            for char in input_text:
-                if char == " ":
-                    output_text += " "
-                else:
-                    output_text += "X"
-            # "Deidentified text should be assigned till here"
-            deidentified_text = DeidentifiedText.objects.create(
-                input_text=input_text,
-                output_text=output_text
-            )
-            return render(request, 'deidentify_demo.html', {'form': form, 'output_text': output_text})
-    else:
-        form = DeidentifyForm()
-        return render(request, 'deidentify_demo.html', {'form': form})
