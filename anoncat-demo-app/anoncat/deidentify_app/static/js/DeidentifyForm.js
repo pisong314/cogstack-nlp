604a5df831249884ecb87365a22a1ba874d2cd66
antsh3k <antshek@hotmail.com>
2023-06-06 15:18:52 +0100

    frontend formatting buttons moved

--- anoncat-demo-app/anoncat/deidentify_app/static/js/DeidentifyForm.js
+++ anoncat-demo-app/anoncat/deidentify_app/static/js/DeidentifyForm.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import deidCat from '../media/deidcat.png';
+
+// Create a custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3', // Replace with your desired blue color
+    },
+  },
+});
 
 class DeidentifyForm extends Component {
   state = {
@@ -13,12 +24,10 @@ class DeidentifyForm extends Component {
     event.preventDefault();
 
     const { inputText, redact } = this.state;
-    //const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
     const formData = new FormData();
     formData.append('input_text', inputText);
     formData.append('redact', redact);
-    //formData.append('csrfmiddlewaretoken', csrfToken);
 
     try {
       const response = await fetch('/', {
@@ -47,46 +56,54 @@ class DeidentifyForm extends Component {
     const { inputText, redact, outputText } = this.state;
 
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-grid">
-            <div className="form-item document-container">
-              <textarea
-                rows={40}
-                name="inputText"
-                value={inputText}
-                onChange={this.handleChange}
-                className="input-text"
-                style={{ width: '100%', resize: 'none', }}
-                placeholder="Enter text here"
-              />
-            </div>
-            <div className="form-item center-column">
-              <img src={deidCat} alt="DeIDCAT logo" className="image-size" />
-              <button type="submit">Deidentify</button>
-              <div className="redact-checkbox">
-                <label>
-                  Redact
-                  <span style={{ marginLeft: '9px' }}></span>
-                  <input
-                    type="checkbox"
-                    name="redact"
-                    checked={redact}
-                    onChange={this.handleChange}
-                  />
-                </label>
+      <ThemeProvider theme={theme}>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-grid">
+              <div className="form-item document-container">
+                <textarea
+                  rows={40}
+                  name="inputText"
+                  value={inputText}
+                  onChange={this.handleChange}
+                  className="input-text"
+                  style={{ width: '100%', resize: 'none', }}
+                  placeholder="Enter text here"
+                />
+              </div>
+              <div className="form-item center-column">
+                <div className="logo-container">
+                  <img src={deidCat} alt="DeIDCAT logo" className="image-size" />
+                </div>
+                <div className="button-container">
+                  <Button variant="contained" color="primary" type="submit">
+                    Deidentify
+                  </Button>
+                </div>
+                <div className="redact-checkbox">
+                  <label>
+                    Redact
+                    <span style={{ marginLeft: '9px' }}></span>
+                    <input
+                      type="checkbox"
+                      name="redact"
+                      checked={redact}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="form-item document-container output-text">
+                {outputText ? (
+                  <p> {outputText}</p>
+                ) : (
+                  <p className="output-text-default">Deidentification Demo</p>
+                )}
               </div>
             </div>
-            <div className="form-item document-container output-text">
-              {outputText ? (
-                <p> {outputText}</p>
-              ) : (
-                <p className="output-text-default">Deidentification Demo</p>
-              )}
-            </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      </ThemeProvider>
     );
   }
 }
