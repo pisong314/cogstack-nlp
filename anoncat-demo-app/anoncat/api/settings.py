c34c5f00c9283326fc829a667947b7fedce94cfe
tomolopolis <tsearle88@gmail.com>
2024-02-07 12:13:08 +0000

    Add example text via a button, limit allowed hosts to local, fix some styling and limit required deps

--- anoncat-demo-app/anoncat/api/settings.py
+++ anoncat-demo-app/anoncat/api/settings.py
@@ -12,25 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
-
-# Load environment variables from .env file in the BASE_DIR
-dotenv_path = BASE_DIR / '.env'
-load_dotenv(dotenv_path)
 
 # Enter your model name here:
 model_name = os.environ.get('MODEL_NAME')
 
 if model_name:
     DEID_MODEL = os.path.join(os.path.join(BASE_DIR, 'deidentify_app', 'models', model_name))
-    print(f'The "MODEL_PATH" environment variable is set to: {DEID_MODEL}')
+    print(f'The model to be loaded is: {DEID_MODEL}')
 else:
-    raise ValueError('The "MODEL_PATH" environment is not set. Please consider populating it in the .env')
+    raise ValueError('The "MODEL_NAME" environment is not set. Please consider populating it in the .env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -41,7 +35,7 @@ SECRET_KEY = 'django-insecure-oqmt4bxkxxj=_oas!^#2&k(fs7929ov-_9#bixft_z-=++td%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- anoncat-demo-app/anoncat/deidentify_app/static/js/DeidentifyForm.js
+++ anoncat-demo-app/anoncat/deidentify_app/static/js/DeidentifyForm.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import deidCat from '../media/deidcat.png';
+import {Checkbox} from "@mui/material"
 
 // Create a custom theme
 const theme = createTheme({
@@ -12,6 +13,81 @@ const theme = createTheme({
     },
   },
 });
+
+const exampleText = `
+Patient Information:
+
+Name: John Parkinson
+Date of Birth: February 12, 1958
+Gender: Male
+Address: 789 Wellness Lane, Healthville, HV 56789
+Phone: (555) 555-1234
+Email: john.parkinson@email.com
+Emergency Contact:
+
+Name: Mary Parkinson
+Relationship: Spouse
+Phone: (555) 555-5678
+Insurance Information:
+
+Insurance Provider: HealthWell Assurance
+Policy Number: HW765432109
+Group Number: G876543
+Medical History:
+
+Allergies:
+
+None reported
+Medications:
+
+Levodopa/Carbidopa for Parkinson's disease symptoms
+Pramipexole for restless legs syndrome
+Lisinopril for hypertension
+Atorvastatin for hyperlipidemia
+Metformin for Type 2 Diabetes
+Medical Conditions:
+
+Parkinson's Disease (diagnosed on June 20, 2015)
+Hypertension
+Hyperlipidemia
+Type 2 Diabetes
+Osteoarthritis
+Vital Signs:
+
+Blood Pressure: 130/80 mmHg
+Heart Rate: 72 bpm
+Temperature: 98.4°F
+Respiratory Rate: 18 breaths per minute
+Recent Inpatient Stay (Dates: September 1-10, 2023):
+
+Reason for Admission: Acute exacerbation of Parkinson's symptoms, pneumonia, and uncontrolled diabetes.
+
+Interventions:
+
+Neurology Consultation for Parkinson's disease management adjustments.
+Antibiotic therapy for pneumonia.
+Continuous glucose monitoring and insulin therapy for diabetes control.
+Physical therapy sessions to maintain mobility.
+Complications:
+
+Delirium managed with close monitoring and appropriate interventions.
+Discharge Plan:
+
+Medication adjustments for Parkinson's disease.
+Follow-up appointments with neurologist, endocrinologist, and primary care.
+Home health care for continued physical therapy.
+Follow-up Visits:
+
+Date: October 15, 2023
+
+Reason for Visit: Post-discharge Follow-up
+Notes: Stable Parkinson's symptoms, pneumonia resolved. Adjusted diabetes medications for better control.
+Date: December 5, 2023
+
+Reason for Visit: Neurology Follow-up
+Notes: Fine-tuned Parkinson's medication regimen. Recommended ongoing physical therapy.
+`
+
 
 class DeidentifyForm extends Component {
   state = {
@@ -49,63 +125,68 @@ class DeidentifyForm extends Component {
   handleChange = (event) => {
     const { name, value, type, checked } = event.target;
     const inputValue = type === 'checkbox' ? checked : value;
-    this.setState({ [name]: inputValue });  
+    this.setState({ [name]: inputValue });
   };
+  setExampleText = (_) => {
+    this.setState({inputText: exampleText})
+  }
 
   render() {
     const { inputText, redact, outputText } = this.state;
 
     return (
-      <ThemeProvider theme={theme}>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-grid">
-              <div className="form-item document-container">
+        <ThemeProvider theme={theme}>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-grid">
+                <div className="form-item document-container">
                 <textarea
-                  rows={40}
-                  name="inputText"
-                  value={inputText}
-                  onChange={this.handleChange}
-                  className="input-text"
-                  style={{resize: 'none', }}
-                  placeholder="Enter text here"
+                    rows={40}
+                    name="inputText"
+                    value={inputText}
+                    onChange={this.handleChange}
+                    className="input-text"
+                    style={{resize: 'none', }}
+                    placeholder="Enter text here"
                 />
+                </div>
+                <div className="form-item center-column">
+                  <div className="logo-container">
+                    <img src={deidCat} alt="DeIDCAT logo" className="image-size" />
+                  </div>
+                  <div className="button-container">
+                    <Button variant="contained" color="primary" type="submit">
+                      Deidentify
+                    </Button>
+                  </div>
+                  <div className="redact-checkbox">
+                    <label>
+                      Redact
+                      <span style={{ marginLeft: '9px' }}></span>
+                      <Checkbox
+                          name="redact"
+                          checked={redact}
+                          onChange={this.handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div>
+                    <Button color="secondary" variant="contained" onClick={this.setExampleText}>Use Example Text</Button>
+                  </div>
+                </div>
+                <div className="form-item document-container output-text">
+                  <div className="output-text-container">
+                    {outputText ? (
+                        <p> {outputText}</p>
+                    ) : (
+                        <p className="output-text-default">Deidentification Demo</p>
+                    )}
+                  </div>
+                </div>
               </div>
-              <div className="form-item center-column">
-                <div className="logo-container">
-                  <img src={deidCat} alt="DeIDCAT logo" className="image-size" />
-                </div>
-                <div className="button-container">
-                  <Button variant="contained" color="primary" type="submit">
-                    Deidentify
-                  </Button>
-                </div>
-                <div className="redact-checkbox">
-                  <label>
-                    Redact
-                    <span style={{ marginLeft: '9px' }}></span>
-                    <input
-                      type="checkbox"
-                      name="redact"
-                      checked={redact}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                </div>
-              </div>
-              <div className="form-item document-container output-text">
-                <div className="output-text-container">
-                  {outputText ? (
-                  <p> {outputText}</p>
-                  ) : (
-                  <p className="output-text-default">Deidentification Demo</p>
-                  )}
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </ThemeProvider>
+            </form>
+          </div>
+        </ThemeProvider>
     );
   }
 }
