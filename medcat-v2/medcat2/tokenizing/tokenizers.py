ba1b648aa922867e9b4cef26f90a3d9fb72fb688
mart-r <mart.ratas@gmail.com>
2024-10-24 11:35:11 +0100

    Add listing of tokenizers and rename creation/registration methods for tokenizing

--- medcat-v2/medcat2/tokenizing/tokenizers.py
+++ medcat-v2/medcat2/tokenizing/tokenizers.py
@@ -22,11 +22,15 @@ _TOKENIZERS_REGISTRY = Registry(BaseTokenizer,  # type: ignore
                                 lazy_defaults=_DEFAULT_TOKENIZING)
 
 
-def get_tokenizer(tokenizer_name: str, *args, **kwargs) -> BaseTokenizer:
+def create_tokenizer(tokenizer_name: str, *args, **kwargs) -> BaseTokenizer:
     return _TOKENIZERS_REGISTRY.get_component(tokenizer_name)(*args, **kwargs)
 
 
-def register_platform(name: str, clazz: Type[BaseTokenizer]) -> None:
+def list_available_tokenizers() -> list[tuple[str, str]]:
+    return _TOKENIZERS_REGISTRY.list_components()
+
+
+def register_tokenizer(name: str, clazz: Type[BaseTokenizer]) -> None:
     _TOKENIZERS_REGISTRY.register(name, clazz)
     logger.debug("Registered tokenizer '%s': '%s.%s'",
                  name, clazz.__module__, clazz.__name__)
