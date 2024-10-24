6a408d4bfb4721d6bbbc5ce0bfc3a69b4b624ffa
mart-r <mart.ratas@gmail.com>
2024-10-24 16:03:28 +0100

    Move default arg creation to use CDB instead of just the config

--- medcat-v2/medcat2/utils/default_args.py
+++ medcat-v2/medcat2/utils/default_args.py
@@ -4,7 +4,8 @@ components creation.
 """
 from typing import cast
 
-from medcat2.config.config import Config, CoreComponentConfig
+from medcat2.config.config import CoreComponentConfig
+from medcat2.cdb import CDB
 
 import logging
 
@@ -12,14 +13,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def set_defaults(config: Config):
-    nlp_cnf = config.general.nlp
+def set_defaults(cdb: CDB):
+    nlp_cnf = cdb.config.general.nlp
     if nlp_cnf.provider == 'spacy':
         logging.debug("Setting default arguments for spacy constructor")
         from medcat2.tokenizing.spacy_impl.tokenizers import (
             set_def_args_kwargs)
-        set_def_args_kwargs(config)
-    for comp_name, comp_config in config.components:
+        set_def_args_kwargs(cdb.config)
+    for comp_name, comp_config in cdb.config.components:
         comp_cnf = cast(CoreComponentConfig, comp_config)
         if comp_cnf.comp_name != 'default':
             continue
@@ -27,5 +28,5 @@ def set_defaults(config: Config):
                       comp_name)
         if comp_name == "tagging":
             from medcat2.components.tagging import tagger
-            tagger.set_def_args_kwargs(config)
+            tagger.set_def_args_kwargs(cdb.config)
         # TODO: other components
