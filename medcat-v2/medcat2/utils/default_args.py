7d7e402a3d65f4bde2f10b0ef0cf9909c8e3ffd4
mart-r <mart.ratas@gmail.com>
2024-10-24 13:23:28 +0100

    Update default argument setter to set tagger arguments

--- medcat-v2/medcat2/utils/default_args.py
+++ medcat-v2/medcat2/utils/default_args.py
@@ -2,7 +2,7 @@
 in the config for the default tokenizer and the defualt
 components creation.
 """
-from medcat2.config import Config
+from medcat2.config.config import Config, CoreComponentConfig
 
 import logging
 
@@ -17,6 +17,13 @@ def set_defaults(config: Config):
         from medcat2.tokenizing.spacy_impl.tokenizers import (
             set_def_args_kwargs)
         set_def_args_kwargs(config)
-    # TODO: set defaults of components
-    # for comp_name, comp_cnf in config.components:
-    #     pass
+    for comp_name, comp_cnf in config.components:
+        comp_cnf: CoreComponentConfig
+        if comp_cnf.comp_name != 'default':
+            continue
+        logging.debug("Setting default arguments for component '%s'",
+                      comp_name)
+        if comp_name == "tagging":
+            from medcat2.components.tagging import tagger
+            tagger.set_def_args_kwargs(config)
+        # TODO: other components
