4c41bfb005dbaf29ff03c7fa4b43504f8e6ecb86
mart-r <mart.ratas@gmail.com>
2024-10-25 10:17:04 +0100

    Add postprocessor to separated final/nested ents

---
+++ medcat-v2/medcat2/utils/postprocessing.py
@@ -0,0 +1,26 @@
+from medcat2.tokenizing.tokenizers import MutableDocument
+
+
+# NOTE: the following used (in medcat v1) check tuis
+#       but they were never passed to the method so
+#       I've omitted it now
+def create_main_ann(doc: MutableDocument) -> None:
+    """Creates annotation in the spacy ents list
+    from all the annotations for this document.
+
+    Args:
+        doc (Doc): Spacy document.
+    """
+    doc.all_ents.sort(key=lambda x: len(x.base.text), reverse=True)
+    tkns_in = set()
+    main_anns = []
+    for ent in doc.all_ents:
+        to_add = True
+        for tkn in ent:
+            if tkn in tkns_in:
+                to_add = False
+        if to_add:
+            for tkn in ent:
+                tkns_in.add(tkn)
+            main_anns.append(ent)
+    doc.final_ents = list(doc.final_ents) + main_anns  # type: ignore
