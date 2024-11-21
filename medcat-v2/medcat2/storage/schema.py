62e36a63f3884db05334d8a2434e9b8caa8f1088
mart-r <mart.ratas@gmail.com>
2024-11-21 16:32:53 +0000

    Update storage schema to only include class name/path

--- medcat-v2/medcat2/storage/schema.py
+++ medcat-v2/medcat2/storage/schema.py
@@ -3,7 +3,6 @@ import json
 
 
 _CLASS_PATH = "serialised-class"
-_PARTS_PATH = "serialised-parts"
 
 # hidden file so that it doesn't get overwritten by other things
 DEFAULT_SCHEMA_FILE = ".schema.json"
@@ -13,36 +12,35 @@ def _cls2path(cls: Type) -> str:
     return f"{cls.__module__}.{cls.__name__}"
 
 
-def save_schema(file_name: str, cls: Type, schema: dict[str, str]) -> None:
+def save_schema(file_name: str, cls: Type) -> None:
     """Saves the schema of a class to the specified file.
 
     Args:
         file_name (str): The file to save to.
         cls (Type): The class in question
-        schema (dict[str, str]): The schema mapping attributes
+        schema (dict[str, tuple[str, str]]): The schema mapping attributes
             to the appropriate file names.
     """
     out_data = {
         _CLASS_PATH: _cls2path(cls),
-        _PARTS_PATH: schema,
     }
     with open(file_name, 'w') as f:
         json.dump(out_data, f)
 
 
-def load_schema(file_name: str) -> tuple[str, dict[str, str]]:
+def load_schema(file_name: str) -> str:
     """Loads the schema for a folder of deserialisable files from the file.
 
     Args:
         file_name (str): The schema file
 
     Returns:
-        tuple[str, dict[str, str]]: The class package/name along with
-            the mapping from attribute to file name.
+        tuple[str, dict[str, tuple[str, str]]]: The class package/name along
+            with the mapping from attribute to file name.
     """
     with open(file_name) as f:
         data = json.load(f)
-    return data[_CLASS_PATH], data[_PARTS_PATH]
+    return data[_CLASS_PATH]
 
 
 class IllegalSchemaException(ValueError):
