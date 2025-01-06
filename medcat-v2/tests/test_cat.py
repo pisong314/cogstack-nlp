49adaed55ec77a7441bde9d054f8abd84e69f43d
mart-r <mart.ratas@gmail.com>
2025-01-06 11:42:29 +0000

    Add model creation tests

--- medcat-v2/tests/test_cat.py
+++ medcat-v2/tests/test_cat.py
@@ -1,4 +1,13 @@
+import os
+import pandas as pd
+import json
+
 from medcat2 import cat
+from medcat2.vocab import Vocab
+from medcat2.config import Config
+from medcat2.model_creation.cdb_maker import CDBMaker
+from medcat2.cdb import CDB
+from medcat2.cat import CAT
 
 import unittest
 
@@ -32,3 +41,71 @@ class InferenceFromLoadedTests(TrainedModelTests):
         for nr, ent in enumerate(ents.values()):
             with self.subTest(f"{nr}"):
                 ConvertedFunctionalityTests.assert_has_ent(ent)
+
+
+class CATCReationTests(unittest.TestCase):
+    EXPECT_TRAIN = {}
+
+    # paths
+    VOCAB_DATA_PATH = os.path.join(
+        os.path.dirname(__file__), 'resources', 'vocab_data.txt'
+    )
+    CDB_PREPROCESSED_PATH = os.path.join(
+        os.path.dirname(__file__), 'resources', 'preprocessed4cdb.txt'
+    )
+
+    @classmethod
+    def setUpClass(cls):
+
+        # vocab
+
+        vocab = Vocab()
+        vocab.add_words(cls.VOCAB_DATA_PATH)
+
+        # CDB
+        config = Config()
+
+        maker = CDBMaker(config)
+
+        cls.cdb: CDB = maker.prepare_csvs([cls.CDB_PREPROCESSED_PATH])
+
+        # CAT
+        cls.cat = CAT(cls.cdb, vocab)
+
+    @classmethod
+    def get_cui2ct(cls, cat: CAT | None = None):
+        if cat is None:
+            cat = cls.cat
+        return {
+            cui: info.count_train for cui, info in cat.cdb.cui2info.items()
+            if info.count_train}
+
+    def test_has_expected_training(self):
+        self.assertEqual(self.get_cui2ct(), self.EXPECT_TRAIN)
+
+
+class CATUnsupTrainingTests(CATCReationTests):
+    SELF_SUPERVISED_DATA_PATH = os.path.join(
+        os.path.dirname(__file__), 'resources', 'selfsupervised_data.txt'
+    )
+    EXPECT_TRAIN = {'C01': 2, 'C02': 2, 'C03': 2, 'C04': 1, 'C05': 1}
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        data = pd.read_csv(cls.SELF_SUPERVISED_DATA_PATH)
+        cls.cat.trainer.train_unsupervised(data.text.values)
+
+
+class CATSupTrainingTests(CATUnsupTrainingTests):
+    SUPERVISED_DATA_PATH = os.path.join(
+        os.path.dirname(__file__), 'resources', 'supervised_mct_export.json'
+    )
+    EXPECT_TRAIN = {'C01': 6, 'C02': 7, 'C03': 6, 'C04': 7, 'C05': 7}
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        with open(cls.SUPERVISED_DATA_PATH) as f:
+            data = json.load(f)
+        cls.cat.trainer.train_supervised_raw(data)
